df60bfb6f6efae8664de4de668db6d0e22f811dc
Khalilullah Nuraini <101534144+khalilaah15@users.noreply.github.com>
2024-09-26 17:46:13 +0700

    third

--- Soal2.cpp
+++ Soal2.cpp
@@ -7,16 +7,19 @@ int main() {
     string fullName;
     double jamKerja; 
     double tarif;
+    double gajiTotal;
 
     cout << "Masukkan Nama Lengkap Karyawan: ";
     getline(cin, fullName);
     cout << "Masukkan Durasi Jam Kerja: ";
     cin >> jamKerja;
+    cout << "Masukkan Durasi Tarif perjam: ";
+    cin >> tarif;
 
-    tarif = jamKerja * 100;
+    gajiTotal = jamKerja * tarif;
 
     cout << right << setw(25) << "Nama Karyawan  |" << setw(25) << "Durasi Jam Kerja  |" << setw(25) << "Gaji Karyawan" << endl;
-    cout << right << setw(22) << fullName << "  |" << setw(22) << jamKerja << "  |" << setw(20) << tarif << endl;
+    cout << right << setw(22) << fullName << "  |" << setw(22) << jamKerja << "  |" << setw(20) << gajiTotal << endl;
 
     return 0;
 }
